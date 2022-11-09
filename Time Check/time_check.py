e3ea846477712c692d188a46886c8953d40e4de1
snyperf1 <snypershriboi@gmail.com>
2022-11-09 15:18:19 +0800

    Update time_check.py

--- Time Check/time_check.py
+++ Time Check/time_check.py
@@ -5,16 +5,10 @@ Darr0 = D.split("/")
 Darr1 = [Darr0[1], Darr0[0], Darr0[2]]
 current = str("/".join(Darr1)+" "+T)
 Dcheck = input("Please enter date in the format DD/MM/YY: ")
-Tcheck = input("Please enter time in the format HH:MM: ")
-if Dcheck[1] == "/":
-    print("fix")
-    print(Dcheck)
-    Dcheck = " "+Dcheck
-    print(Dcheck)
+Tcheck = input("Please enter time in the format HH:MM (24-hour time): ")
 check = Dcheck +" "+ Tcheck
 if current != check:
     while int(time.strftime("%S")) != 0:
-        print("pew")
         time.sleep(1)
 while current != check:
     D = time.strftime("%D")
@@ -22,8 +16,6 @@ while current != check:
     Darr0 = D.split("/")
     Darr1 = [Darr0[1], Darr0[0], Darr0[2]]
     current = str("/".join(Darr1)+" "+T)
-    print(current)
-    print(check)
     if current == check: break
     time.sleep(60)
 print("Uploading now")

9aee820d1f27104363d84f89ffb776fd30231290
aloyisvvcool <88279183+aloyisvvcool@users.noreply.github.com>
2023-01-19 23:19:34 +0800

    added app

--- app.py
+++ app.py
@@ -1,76 +1,77 @@
 import tkinter
 import customtkinter
+from datetime import datetime
+from tkinter import filedialog as fd
 
+def convert_number_to_date(number):
+    date_string = datetime.strptime(number, '%d%m%Y').strftime('%d %B %Y')
+    return date_string
 customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 app = customtkinter.CTk()
-app.geometry("400x780")
-app.title("CustomTkinter simple_example.py")
+app.geometry("720x720")
+app.title("APP")
 
+def youtubecheck():
+    print(youtubecb.get())
 
-def button_callback():
-    print("Button click", combobox_1.get())
+def twittercheck():
+    print(twittercb.get())
 
+def tiktokcheck():
+    print(tiktokcb.get())
 
-def slider_callback(value):
-    progressbar_1.set(value)
+def instacheck():
+    print(instacb.get())
 
-def testfunc():
-    print(switch_1.get())
+def titleadd():
+    print(title_entry.get())
 
+def dateadd():
+    date_string = datetime.strptime(date_entry.get(), '%d%m%Y').strftime('%d %B %Y')
+    print(date_string)
+
+def choose_file():
+    print(str(fd.askopenfile()))
+
+def descriptionadd():
+    print(caption_entry.getvar())
 
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT)
+label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text='New Post')
 label_1.pack(pady=10, padx=10)
 
-progressbar_1 = customtkinter.CTkProgressBar(master=frame_1)
-progressbar_1.pack(pady=10, padx=10)
+youtubecb = customtkinter.CTkCheckBox(master=frame_1, command=youtubecheck, text='Youtube')
+youtubecb.pack(pady=10, padx=10)
 
-button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback)
-button_1.pack(pady=10, padx=10)
+twittercb = customtkinter.CTkCheckBox(master=frame_1, command=twittercheck, text='Twitter')
+twittercb.pack(pady=10, padx=10)
 
-slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
-slider_1.pack(pady=10, padx=10)
-slider_1.set(0.5)
+tiktokcb = customtkinter.CTkCheckBox(master=frame_1, command=tiktokcheck, text='Tiktok')
+tiktokcb.pack(pady=10, padx=10)
 
-entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="CTkEntry")
-entry_1.pack(pady=10, padx=10)
+instacb = customtkinter.CTkCheckBox(master=frame_1, command=instacheck, text='Instagram')
+instacb.pack(pady=10, padx=10)
 
-optionmenu_1 = customtkinter.CTkOptionMenu(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-optionmenu_1.pack(pady=10, padx=10)
-optionmenu_1.set("CTkOptionMenu")
+title_entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Title")
+title_entry.pack(pady=10, padx=10)
+title_confirm = customtkinter.CTkButton(master=frame_1, command=titleadd, text='Choose Title')
+title_confirm.pack(pady=10, padx=10)
 
-combobox_1 = customtkinter.CTkComboBox(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-combobox_1.pack(pady=10, padx=10)
-combobox_1.set("CTkComboBox")
+caption_entry = customtkinter.CTkTextbox(master=frame_1, height=200)
+caption_entry.pack(pady=10, padx=10)
+caption_confirm = customtkinter.CTkButton(master=frame_1, command=descriptionadd, text='Choose Description')
+caption_confirm.pack(pady=10, padx=10)
 
-checkbox_1 = customtkinter.CTkCheckBox(master=frame_1)
-checkbox_1.pack(pady=10, padx=10)
+date_entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="DDMMYYYY")
+date_entry.pack(pady=10, padx=10)
+date_confirm = customtkinter.CTkButton(master=frame_1, command=dateadd, text='Add Date')
+date_confirm.pack(pady=10, padx=10)
 
-radiobutton_var = tkinter.IntVar(value=1)
-
-radiobutton_1 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=1)
-radiobutton_1.pack(pady=10, padx=10)
-
-radiobutton_2 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=2)
-radiobutton_2.pack(pady=10, padx=10)
-
-switch_1 = customtkinter.CTkSwitch(master=frame_1, command=testfunc)
-switch_1.pack(pady=10, padx=10)
-
-text_1 = customtkinter.CTkTextbox(master=frame_1, width=200, height=70)
-text_1.pack(pady=10, padx=10)
-text_1.insert("0.0", "CTkTextbox\n\n\n\n")
-
-segmented_button_1 = customtkinter.CTkSegmentedButton(master=frame_1, values=["CTkSegmentedButton", "Value 2"])
-segmented_button_1.pack(pady=10, padx=10)
-
-tabview_1 = customtkinter.CTkTabview(master=frame_1, width=200, height=70)
-tabview_1.pack(pady=10, padx=10)
-tabview_1.add("CTkTabview")
-tabview_1.add("Tab 2")
+file_select = customtkinter.CTkButton(master=frame_1, command=choose_file, text='Select File')
+file_select.pack(pady=10, padx=10)
 
 app.mainloop()

23e6d85fef65f9731b1b5c0bb04f4406e2683eaf
aloyisvvcool <88279183+aloyisvvcool@users.noreply.github.com>
2023-02-06 21:27:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import customtkinter
 from datetime import datetime
 from tkinter import filedialog as fd
 
-def convert_number_to_date(number):
-    date_string = datetime.strptime(number, '%d%m%Y').strftime('%d %B %Y')
-    return date_string
 customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -13,6 +10,9 @@ app = customtkinter.CTk()
 app.geometry("720x720")
 app.title("APP")
 
+def convert_number_to_date(number):
+    date_string = datetime.strptime(number, '%d%m%Y').strftime('%d %B %Y')
+    return date_string
 def youtubecheck():
     print(youtubecb.get())
 
@@ -25,13 +25,31 @@ def tiktokcheck():
 def instacheck():
     print(instacb.get())
 
+titleadded = False
 def titleadd():
-    print(title_entry.get())
+    global titleadded
+    if titleadded == True:
+        destroy_title = lambda: title.destroy()
+        destroy_title()
+    title_name = title_entry.get()
+    global title
+    title=customtkinter.CTkLabel(app, text=f'Title: {title_name}', font=('Helvetica', 15))
+    title.place(x=70,y=300)
+    titleadded = True
 
-def dateadd():
-    date_string = datetime.strptime(date_entry.get(), '%d%m%Y').strftime('%d %B %Y')
-    print(date_string)
-
+captionadded = False
+def descriptionadd():
+    global captionadded
+    if captionadded == True:
+        destroy_caption = lambda: caption.destroy()
+        destroy_caption()
+    caption_name = caption_entry.get("1.0", 'end-1c')
+    global caption
+    caption=customtkinter.CTkLabel(app, text=f'Caption: {caption_name}', font=('Helvetica', 15))
+    caption.place(x=70,y=430)
+    captionadded = True
+    
+timeadded = False
 def timeadd():
     time_str = time_entry.get()
     if not time_str.isdigit() or len(time_str) != 4:
@@ -48,20 +66,61 @@ def timeadd():
     else:
         hour -= 12
         am_pm = "PM"
-    print(f"{hour}:{minute:02d} {am_pm}")
+    formatted_time = f"{hour}:{minute:02d} {am_pm}"
 
+    global timeadded
+    if timeadded == True:
+        destroy_time = lambda: time.destroy()
+        destroy_time()
+    formatted_time = f"{hour}:{minute:02d} {am_pm}"
+    global time
+    time=customtkinter.CTkLabel(app, text=f'Time: {formatted_time}', font=('Helvetica', 15))
+    time.place(x=70,y=330)
+    timeadded = True
 
+dateadded = False
+def dateadd():
+    date_string = datetime.strptime(date_entry.get(), '%d%m%Y').strftime('%d %B %Y')
+    print(date_string)
+    global dateadded
+    if dateadded == True:
+        destroy_date = lambda: date.destroy()
+        destroy_date()
+    global date
+    date=customtkinter.CTkLabel(app, text=f'Date: {date_string}', font=('Helvetica', 15))
+    date.place(x=70,y=360)
+    dateadded = True
+
+fileadded = False
 def choose_file():
-    print(str(fd.askopenfile())[25:-28])
+    file_path = str(fd.askopenfile())[25:-28]
+    print(file_path)
+    if len(file_path) > 30:
+        file_path = file_path[:30] + "\n" + file_path[30:]
+    global fileadded
+    if fileadded == True:
+        destroy_file = lambda: file.destroy()
+        destroy_file()
+    global file
+    file=customtkinter.CTkLabel(app, text=f'File: {file_path}', font=('Helvetica', 15))
+    file.place(x=70,y=390)
+    fileadded = True
 
-def descriptionadd():
-    print(caption_entry.get("1.0", 'end-1c'))
+def post():
+    if youtubecb.get(): #youtube upload code here
+        pass
+    if twittercb.get(): #twitter upload code here
+        pass
+    if tiktokcb.get(): #tiktok upload code here
+        pass
+    if instacb.get(): #instagram upload code here
+        pass
 
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
 #fix this lol
-
+'''
 app.tabview = customtkinter.CTkTabview(app, width=720, height=720)
 app.tabview.place(x=0 , y=0)
 app.tabview.add("CTkTabview")
@@ -71,43 +130,48 @@ app.tabview.tab("CTkTabview").grid_columnconfigure(0, weight=1)  # configure gri
 app.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
 app.label_tab_2 = customtkinter.CTkLabel(app.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
 app.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
-
-label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text='New Post')
-label_1.place(x=200,y=10)
-
-youtubecb = customtkinter.CTkCheckBox(master=frame_1, command=youtubecheck, text='Youtube')
-youtubecb.place(x=10,y=50)
-
-twittercb = customtkinter.CTkCheckBox(master=frame_1, command=twittercheck, text='Twitter')
-twittercb.place(x=10,y=100)
-
-tiktokcb = customtkinter.CTkCheckBox(master=frame_1, command=tiktokcheck, text='Tiktok')
-tiktokcb.place(x=10,y=150)
-
-instacb = customtkinter.CTkCheckBox(master=frame_1, command=instacheck, text='Instagram')
-instacb.place(x=10,y=200)
+'''
 
 title_entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Title")
-title_entry.place(x=300,y=50)
+title_entry.place(x=300,y=70)
 title_confirm = customtkinter.CTkButton(master=frame_1, command=titleadd, text='Choose Title')
-title_confirm.place(x=300,y=90)
+title_confirm.place(x=300,y=110)
 
 caption_entry = customtkinter.CTkTextbox(master=frame_1, height=200)
-caption_entry.place(x=300,y=140)
+caption_entry.place(x=300,y=160)
 caption_confirm = customtkinter.CTkButton(master=frame_1, command=descriptionadd, text='Choose Description')
-caption_confirm.place(x=300,y=350)
+caption_confirm.place(x=300,y=370)
 
 time_entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="HHMM")
-time_entry.place(x=300,y=400)
+time_entry.place(x=300,y=420)
 time_confirm = customtkinter.CTkButton(master=frame_1, command=timeadd, text='Add Time')
-time_confirm.place(x=300,y=440)
+time_confirm.place(x=300,y=460)
 
 date_entry = customtkinter.CTkEntry(master=frame_1, placeholder_text="DDMMYYYY")
-date_entry.place(x=300,y=490)
+date_entry.place(x=300,y=510)
 date_confirm = customtkinter.CTkButton(master=frame_1, command=dateadd, text='Add Date')
-date_confirm.place(x=300,y=530)
+date_confirm.place(x=300,y=550)
 
 file_select = customtkinter.CTkButton(master=frame_1, command=choose_file, text='Select Video File')
-file_select.place(x=300,y=580)
+file_select.place(x=300,y=590)
+
+post_button = customtkinter.CTkButton(master=frame_1, command=post, text='Post!')
+post_button.place(x=300,y=630)
+
+heading = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text='New Post', font=('Helvetica', 30))
+heading.place(x=230,y=10)
+
+youtubecb = customtkinter.CTkCheckBox(master=frame_1, command=youtubecheck, text='Youtube', font=('Helvetica', 20))
+youtubecb.place(x=70,y=70)
+
+twittercb = customtkinter.CTkCheckBox(master=frame_1, command=twittercheck, text='Twitter', font=('Helvetica', 20))
+twittercb.place(x=70,y=120)
+
+tiktokcb = customtkinter.CTkCheckBox(master=frame_1, command=tiktokcheck, text='Tiktok', font=('Helvetica', 20))
+tiktokcb.place(x=70,y=170)
+
+instacb = customtkinter.CTkCheckBox(master=frame_1, command=instacheck, text='Instagram', font=('Helvetica', 20))
+instacb.place(x=70,y=220)
+
 
 app.mainloop()

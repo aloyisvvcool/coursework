31db0882c0c034da472d419b753e2cb4db8d2cf5
snyperf1 <snypershriboi@gmail.com>
2023-02-21 00:33:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ import pyautogui
 import tweepy   #used for Twitter posting. Has been deactivated since Feb 9 2023 when Twitter API was a made paid service :(
 from cryptography.fernet import Fernet  #used for encrypting and decrypting passwords and other sensitive details
 from google_auth_oauthlib.flow import InstalledAppFlow  #Google packages, pandas and os are needed for YouTUbe posting
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build 
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
@@ -37,35 +37,35 @@ def tobytes(x): #function used in decrypting information from file
     return bytes(x,'utf-8')
 
 texts = []  #contains all the encrypted files
-with open('textinfo.txt','r') as f:
-    [texts.append(line.strip()) for line in f.readlines()]
+with open('textinfo.txt','r') as f: #opens the file
+    [texts.append(line.strip()) for line in f.readlines()] #reads the file and adds each line to the list
 
-key = tobytes(texts[0])
+key = tobytes(texts[0]) #gets the key from the file
 fernet = Fernet(key) #casts key to needed type
 
-TWITTER_ACCESS_KEY = fernet.decrypt(tobytes(texts[1])).decode() #creates usable strings from the encoded file
-TWITTER_ACCESS_SECRET = fernet.decrypt(tobytes(texts[2])).decode()
-TWITTER_CONSUMER_KEY = fernet.decrypt(tobytes(texts[3])).decode()
-TWITTER_CONSUMER_SECRET = fernet.decrypt(tobytes(texts[4])).decode()
-TIKTOK_EMAIL = fernet.decrypt(tobytes(texts[5])).decode()
-TIKTOK_PASSWORD = fernet.decrypt(tobytes(texts[6])).decode()
-INSTAGRAM_EMAIL = fernet.decrypt(tobytes(texts[7])).decode()
-INSTAGRAM_PASSWORD = fernet.decrypt(tobytes(texts[8])).decode()
+TWITTER_ACCESS_KEY = fernet.decrypt(tobytes(texts[1])).decode() #decodes the strings, the .decode() is needed since the strings are encrypted as bytes, this line for twitter access key
+TWITTER_ACCESS_SECRET = fernet.decrypt(tobytes(texts[2])).decode() #same as above, but for twitter access secret
+TWITTER_CONSUMER_KEY = fernet.decrypt(tobytes(texts[3])).decode() #same as above, but for twitter consumer key
+TWITTER_CONSUMER_SECRET = fernet.decrypt(tobytes(texts[4])).decode() #same as above, but for twitter consumer secret
+TIKTOK_EMAIL = fernet.decrypt(tobytes(texts[5])).decode() #same as above, but for tiktok email
+TIKTOK_PASSWORD = fernet.decrypt(tobytes(texts[6])).decode() #same as above, but for tiktok password
+INSTAGRAM_EMAIL = fernet.decrypt(tobytes(texts[7])).decode() #same as above, but for instagram email
+INSTAGRAM_PASSWORD = fernet.decrypt(tobytes(texts[8])).decode() #same as above, but for instagram password
 video_title = None  #These 5 variables are initialised, but should be changed later on
 video_caption = None
 upload_time = None
 upload_date = None
-file_path = None
+file_path = None #initialising file_path is especially important for the tiktok code to work properly
 customtkinter.set_appearance_mode("dark")  #sets the app to dark mode, but can get changed in the app
 customtkinter.set_default_color_theme("blue")   #sets the theme of the app
 
 app = customtkinter.CTk()
-app.geometry("720x720") #sets the dimensions of the app
-app.title("Sociable") #sets the app name
+app.geometry("720x720") #sets the dimensions of the app window
+app.title("Sociable") #sets the app name to "Sociable"
 
-def convert_number_to_date(number): #function that converts time input into a usable string
-    date_string = datetime.strptime(number, '%d%m%Y').strftime('%d %B %Y')
-    return date_string
+def convert_number_to_date(number): #function that converts the date entered into a string
+    date_string = datetime.strptime(number, '%d%m%Y').strftime('%d %B %Y') #converts the date into a string
+    return date_string #returns the string
 
 def youtubecheck(): #function that checks if the youtube box is ticked, is empty since we don't use it but is needed to create the checkbox
     pass
@@ -80,108 +80,108 @@ def instacheck(): #function that checks if the instagram box is ticked, is empty
     pass
 
 titleadded = False  #placeholder variable, only there so that title can be displayed once entered
-def titleadd():
-    global titleadded
-    global video_title
+def titleadd(): #function that adds the title to the app
+    global titleadded #makes the variable global so that it can be used outside of the function
+    global video_title #makes the variable global so that it can be used outside of the function
     if titleadded == True: #if a title has been previously added, remove it
         destroy_title = lambda: title.destroy()
-        destroy_title()
+        destroy_title() #removes the title
     title_name = title_entry.get() #assign the title that has been entered to variable
-    video_title = title_name
-    global title
+    video_title = title_name #assign the title that has been entered to
+    global title #makes the variable global so that it can be used outside of the function
     title=customtkinter.CTkLabel(app, text=f'Title: {title_name}', font=('Helvetica', 15)) #create the label for the title
     title.place(x=70,y=300) #place the label
-    titleadded = True
+    titleadded = True #makes the variable true so that the title can't be added again
 
 captionadded = False #placeholder variable, only there so that caption can be displayed once entered
-def descriptionadd():
-    global captionadded
-    global video_caption
+def descriptionadd(): #function that adds the caption to the app
+    global captionadded #makes the variable global so that it can be used outside of the function
+    global video_caption #makes the variable global so that it can be used outside of the function
     if captionadded == True: #if a caption has been previously added, remove it
         destroy_caption = lambda: caption.destroy()
-        destroy_caption()
+        destroy_caption() #removes the caption
     caption_name = caption_entry.get("1.0", 'end-1c') #assign the caption that has been entered to variable
-    video_caption = caption_name
-    global caption
+    video_caption = caption_name #assign the caption that has been entered to
+    global caption #makes the variable global so that it can be used outside of the function
     caption=customtkinter.CTkLabel(app, text=f'Caption: {caption_name}', font=('Helvetica', 15)) #create the label for the caption
     caption.place(x=70,y=430) #place the label
-    captionadded = True
+    captionadded = True #makes the variable true so that the caption can't be added again
     
 timeadded = False #placeholder variable, only there so that time can be displayed once entered
-def timeadd():
-    global upload_time
+def timeadd(): #function that adds the time to the app
+    global upload_time #makes the variable global so that it can be used outside of the function
     time_str = time_entry.get() #assign the time that has been entered to variable
     if not time_str.isdigit() or len(time_str) != 4: #if the time does not fit the needed format of HHMM, reject it
-        raise ValueError("Input should be a 4-digit time string in the format 'HHMM'")
-    else:
+        raise ValueError("Input should be a 4-digit time string in the format 'HHMM'") #raise an error
+    else: #if the time is in the correct format, format it
         hour = int(time_str[:2]) #formats the time input into a usable string
-        minute = int(time_str[2:])
-        formatted_time = f"{hour}:{minute:02d}"
+        minute = int(time_str[2:]) #formats the time input into a usable string
+        formatted_time = f"{hour}:{minute:02d}" #formats the time input into a usable string
 
-    global timeadded
+    global timeadded #makes the variable global so that it can be used outside of the function
     if timeadded == True: #if a time has been previously added, remove it
         destroy_time = lambda: timevar.destroy()
-        destroy_time()
-    formatted_time = f"{hour}:{minute:02d}"
-    upload_time = formatted_time 
-    global timevar
+        destroy_time() #removes the time
+    formatted_time = f"{hour}:{minute:02d}" #formats the time input into a usable string
+    upload_time = formatted_time #assign the time that has been entered to
+    global timevar #makes the variable global so that it can be used outside of the function
     timevar=customtkinter.CTkLabel(app, text=f'Time: {formatted_time}', font=('Helvetica', 15)) #create the label for the time
     timevar.place(x=70,y=330) #place the label
     timeadded = True
 
 dateadded = False #placeholder variable, only there so that date can be displayed once entered
 def dateadd():
-    global upload_date
+    global upload_date #makes the variable global so that it can be used outside of the function
     day = date_entry.get()[:2] #assign the date that has been entered to variables based on day, month and year
-    month = date_entry.get()[2:4]
+    month = date_entry.get()[2:4] 
     year = date_entry.get()[4:]
     upload_date = f"{day}/{month}/{year}" #create a usable string and assign it to variable
     global dateadded
     if dateadded == True: #if a date has been previously added, remove it
         destroy_date = lambda: date.destroy()
-        destroy_date()
-    global date
+        destroy_date() #removes the date
+    global date #makes the variable global so that it can be used outside of the function
     date=customtkinter.CTkLabel(app, text=f'Date: {upload_date}', font=('Helvetica', 15)) #create the date for the time
     date.place(x=70,y=360) #place the label
-    dateadded = True
+    dateadded = True #makes the variable true so that the date can't be added again
 
 fileadded = False #placeholder variable, only there so that file path can be displayed once entered
 def choose_file():
-    file_path = str(fd.askopenfile())[25:-28] #asks user to create a 
-    print(file_path)
+    file_path = str(fd.askopenfile())[25:-28] #gets the file path of the file that has been selected
+    print(file_path) #prints the file path to the console
     if len(file_path) > 25: #if the file path exceeds 25 characters, use the next line
         file_path = file_path[:25] + "\n" + file_path[25:]
-    global fileadded
+    global fileadded #makes the variable global so that it can be used outside of the function
     if fileadded == True: #if a file has been previously added, remove it
         destroy_file = lambda: file.destroy()
-        destroy_file()
-    global file
+        destroy_file() #removes the file path
+    global file #makes the variable global so that it can be used outside of the function
     file=customtkinter.CTkLabel(app, text=f'File: {file_path}', font=('Helvetica', 15)) #create the date for the file path
     file.place(x=70,y=390) #place the label
-    fileadded = True
+    fileadded = True #makes the variable true so that the file path can't be added again
 
 def post(): #function to check if the set time has passed, and then post on various platforms
-    global upload_date
+    global upload_date #makes the variable global so that it can be used outside of the function
     global upload_time
-    date_time_str = str(upload_date) + " " + str(upload_time)
-    end_time = time.strptime(date_time_str, "%d/%m/%Y %H:%M")
-    while True:
-        current_time = time.localtime()
-        if current_time >= end_time:
+    date_time_str = str(upload_date) + " " + str(upload_time) 
+    end_time = time.strptime(date_time_str, "%d/%m/%Y %H:%M") #formats the date and time into a usable string
+    while True: #while loop to check if the time has passed
+        current_time = time.localtime() #gets the current time
+        if current_time >= end_time: #if the time has passed, post on the platforms
             if youtubecb.get(): #posts video on youtube
                 '''
                 Start of Jonathan's Code
                 '''
-                def create_service(client_secret_file, api_name, api_version, *scopes, prefix=''):
-                    CLIENT_SECRET_FILE = client_secret_file
-                    API_SERVICE_NAME = api_name
-                    API_VERSION = api_version
-                    SCOPES = [scope for scope in scopes[0]]
+                def create_service(client_secret_file, api_name, api_version, *scopes, prefix=''): #function to create a service
+                    CLIENT_SECRET_FILE = client_secret_file #assign the client secret file to a variable
+                    API_SERVICE_NAME = api_name #assign the api name to a variable
+                    API_VERSION = api_version #assign the api version to a variable
+                    SCOPES = [scope for scope in scopes[0]] #assign the scopes to a variable
                     
-                    creds = None
-                    working_dir = os.getcwd()
-                    token_dir = 'token files'
-                    token_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.json'
+                    creds = None #assign the credentials to a variable
+                    working_dir = os.getcwd() #assign the working directory to a variable
+                    token_dir = 'token files' #assign the token directory to a variable
+                    token_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.json' #assign the token file to a variable
 
                     ### Check if token dir exists first, if not, create the folder
                     if not os.path.exists(os.path.join(working_dir, token_dir)):
